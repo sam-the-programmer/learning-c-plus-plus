18b9a88408cd03805fcb2811ff0fd766583e935d
sam-the-programmer <85027459+sam-the-programmer@users.noreply.github.com>
2022-06-01 20:15:28 +0100

    update code

--- python/1 - calling-cpp-with-python.cpp
+++
@@ -1,6 +0,0 @@
-#define EXPORT __declspec(dllexport)
-
-extern "C"
-{
-   EXPORT int add2(int num) { return num + 2; }
-}
--- python/1 - calling-cpp-with-python.py
+++
@@ -1,5 +0,0 @@
-import ctypes
-import ctypes.util
-lib = ctypes.cdll.LoadLibrary("./lib.dll")
-
-print(lib.add2(12))
